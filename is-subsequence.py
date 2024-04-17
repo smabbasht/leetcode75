aff78473e0a73d5bcf7bcbd9114dd19f3f91273d
smabbasht <abbastaqvi81@gmail.com>
2024-04-17 12:09:40 +0500

    update Day04 challenge

--- is-subsequence.py
+++ is-subsequence.py
@@ -1,15 +1,9 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # Let n and m hold the lengths of the source and target strings respectively
-        n,m = len(s),len(t)
-        # i and j are the two pointers initiated as 0
-        i = j = 0
-
-        # While loop until pointer has reached the end for either strings traversed
-        while i < n and j < m:
-            # update both pointers if match else update only target pointer j
-            if s[i] == t[j]:
-                i += 1
-            j += 1
-        # If i is the same as source string length at the end of this, we've found a substring!
-        return i == n
+        s_pointer = 0
+        i = 0
+        while i < len(t) and s_pointer < len(s):
+            if s[s_pointer] == t[i]:
+                s_pointer += 1
+            i += 1
+        return s_pointer == len(s)
